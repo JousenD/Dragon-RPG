c96247372fd769e5129cfff52ff84ec63917f7b5
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2018-11-11 19:39:57 +0100

    Navmesh Implemented

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -2,11 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityStandardAssets.Characters.ThirdPerson;
+
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float maxHealthPoints = 100;
+    [SerializeField] float attackRadius = 5f;
 
     float currentHealthPoints = 100f;
+    AICharacterControl aiCharacterControl = null;
+    GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        aiCharacterControl = GetComponent<AICharacterControl>();
+    }
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if(distanceToPlayer <= attackRadius)
+        {
+            aiCharacterControl.SetTarget(player.transform);
+        }
+        else
+        {
+            aiCharacterControl.SetTarget(transform);
+        }
+    }
 
     public float healthAsPercentage
     {
