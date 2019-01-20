1a5ac2063d39963777bfcd504a08d05100957fa9
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2019-01-20 18:19:13 +0100

    Interfaces pt 1

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, Idamageable {
 
     [SerializeField] float maxHealthPoints = 100;
 
@@ -15,6 +15,9 @@ public class Player : MonoBehaviour {
             return currentHealthPoints / (float)maxHealthPoints;
         }
     }
-
+    public void TakeDamage(float damage)
+    {
+        currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+    }
 	
 }
