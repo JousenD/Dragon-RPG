7eab01ba6a5e16d20314dee6fbf73bc52e315db9
Jousen-PC\Jousen <Romero87_2112@hotmail.com>
2019-03-18 23:20:30 +0100

    Dominant Hand linked programatically

--- Assets/Characters/Player/Player.cs
+++ Assets/Characters/Player/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Player : MonoBehaviour, Idamageable {
 
@@ -12,7 +13,7 @@ public class Player : MonoBehaviour, Idamageable {
     [SerializeField] float maxAttackRange = 2f;
 
     [SerializeField] Weapon weaponInUse;
-    [SerializeField] GameObject weaponSocket;
+
 
 
     GameObject currentTarget;
@@ -40,9 +41,19 @@ public class Player : MonoBehaviour, Idamageable {
     private void PutWeaponInHand()
     {
         var weaponPrefab = weaponInUse.GetWeaponPrefab();
-        var weapon = Instantiate(weaponPrefab,weaponSocket.transform);
+        GameObject dominantHand = RequestDominantHand();
+        var weapon = Instantiate(weaponPrefab, dominantHand.transform);
         weapon.transform.localPosition = weaponInUse.gripTransform.localPosition;
         weapon.transform.localRotation = weaponInUse.gripTransform.localRotation;
+    }
+
+    private GameObject RequestDominantHand()
+    {
+        var dominantHands = GetComponentsInChildren<DominantHand>();
+        int numberOfDominantHands = dominantHands.Length;
+        Assert.IsFalse(numberOfDominantHands <= 0, "No DominantHand found on Player, please add one");
+        Assert.IsFalse(numberOfDominantHands > 1, "Multiple DominantHands found on Player, remove all but one");
+        return dominantHands[0].gameObject;
     }
 
     private void RegisterForMouseClick()
